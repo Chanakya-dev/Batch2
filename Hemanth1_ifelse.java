988a6488d49930cd596b30aa34ef44cf3fd50a81
Your Name <your@email.com>
2024-06-22 10:07:25 +0530

    Heman

---
+++ Hemanth1_ifelse.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Hemanth1_ifelse {
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            int a=sc.nextInt();
+
+
+        if(a>=100){
+            System.out.println("Allowed to do party");
+    
+        }
+        else{
+            System.out.println("Not Allowed to do party"); 
+        }
+    
+    }
+}
+    
