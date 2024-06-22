62368f1d8022532830d1f3e837c6656de10c4bbb
nirajpavankumar <nirajpavankumar1282@gmail.com>
2024-06-22 11:37:05 +0530

    pavan

---
+++ pavanwhile.java
@@ -0,0 +1,22 @@
+package Batch2;
+
+import java.util.Scanner;
+
+public class pavanwhile {
+        public static void main(String[] args) {
+        Scanner in = new Scanner (System.in );
+        System.out.println("enter the num");
+        int num = in .nextInt() ;
+          int i = 0;
+       while (i  <num ){
+	   System.out.println("pavan ");
+       i ++;
+
+
+        }
+    }
+        
+    
+}
+
+
