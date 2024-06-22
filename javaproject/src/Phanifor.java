f813ef3475f7cf33960821313a1238c41e5d1b2f
gogineniphanidhar <gogineniphanidhar@gmail.com>
2024-06-22 10:27:56 +0530

    phani

---
+++ javaproject/src/Phanifor.java
@@ -0,0 +1,12 @@
+public class Phanifor {
+    public static void main(String[] args) {
+        //for loop
+        for(int i=1;i<=10;i++){
+            for(int j=1;j<=10;j++){
+                System.out.println(i*j+"\t");
+            }
+            System.out.println();
+        }
+    }
+    
+}
