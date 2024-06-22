fd3ef98d2f503da7c21c69708a21376995bb11ea
hemant1317 <hemant1317@gmail.com>
2024-06-22 11:36:46 +0530

    committing for day-5 assignment

---
+++ day-5_project_java/src/HemantReddy_whileloop.java
@@ -0,0 +1,12 @@
+public class Hemantreddy_whileloop {
+    
+    public static void main(String[] args) {
+        
+        int count = 0;
+        while (count < 10) {
+            System.out.println("Hemant " + count);
+            count++;
+
+    }
+}
+}
