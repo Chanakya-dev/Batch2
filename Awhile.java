967c439b52a92cbeee58712b73ae3d4dde0ac487
kashifakibria <kashifakibria@gmail.com>
2024-06-22 10:40:06 +0530

    while

---
+++ Awhile.java
@@ -0,0 +1,10 @@
+package Batch2;
+
+public class Awhile {
+    
+    int decrement=100;
+    while(decrement<100){
+        System.out.println("While count : "+ decrement);
+        decrement--;
+}
+}
